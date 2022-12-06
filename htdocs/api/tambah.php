df5900573814d3e75579b984e565c8e3a4aa0d81
Lannzu Sambool <azizzulia1@gmail.com>
2022-12-06 18:12:16 +0700

    Zulia Aziz Nugroho - Shift G

---
+++ htdocs/api/tambah.php
@@ -0,0 +1,23 @@
+<?php
+require 'koneksi.php';
+
+$input = file_get_contents('php://input');
+$data = json_decode($input, true);
+
+$pesan = [];
+$nim = trim($data['nim']);
+$nama = trim($data['nama']);
+$alamat = trim($data['alamat']);
+
+$query = mysqli_query($koneksi, "insert into siswa(nim, nama, alamat) values('$nim', '$nama', '$alamat')");
+
+if ($query) {
+    http_response_code(201);
+    $pesan['status'] = 'sukses';
+} else {
+    http_response_code(422);
+    $pesan['status'] = 'gagal';
+}
+
+echo json_encode($pesan);
+echo mysqli_error($koneksi);
